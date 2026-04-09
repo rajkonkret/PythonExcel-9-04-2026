08e901b218d239d272a1ac171793933aeb16c31c
Szkolenie_54 <rajkonkret660@gmail.com>
2026-04-09 14:23:37 +0200

    [1 28] update ex2.py

--- day1/ex2.py
+++ day1/ex2.py
@@ -5,3 +5,8 @@ ws = wb.active
 
 print(wb)  # <openpyxl.workbook.workbook.Workbook object at 0x000001D288501550>
 print(ws)  # <Worksheet "vgsales">
+
+ws = wb['vgsales']  # wybieramy arkusz do działąń
+
+print("Total number of rows:", ws.max_row)  # Total number of rows: 16328
+print("Total number of columns:", ws.max_column)  # Total number of columns: 10
