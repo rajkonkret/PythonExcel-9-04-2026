c76e795383c3763e2204e9c8658e9b4ac4c5bebb
Szkolenie_54 <rajkonkret660@gmail.com>
2026-04-09 15:06:10 +0200

    [1 39] update ex4.py

--- day1/ex4.py
+++ day1/ex4.py
@@ -4,3 +4,20 @@ filename = "video2.xlsx"
 
 wb = openpyxl.load_workbook(filename)
 ws = wb['vgsales']
+
+row_position = 1
+
+for i in range(1, ws.max_row):
+    row_position += 1  # row = row + 1
+
+    NA_Sales = ws.cell(row=row_position, column=7).value
+    EU_Sales = ws.cell(row=row_position, column=8).value
+    JP_Sales = ws.cell(row=row_position, column=9).value
+    Other_Sales = ws.cell(row=row_position, column=10).value
+
+    total_series = (NA_Sales + EU_Sales + JP_Sales + Other_Sales)
+    ws.cell(row=row_position, column=11).value = total_series
+
+# wykona się tylko raz
+wb.save(filename)
+wb.close()
