724cf40e4cef6e5270e3f958861fb363c549a735
Szkolenie_54 <rajkonkret660@gmail.com>
2026-04-10 10:19:39 +0200

    [2 19] update ex7

--- day2/ex7.py
+++ day2/ex7.py
@@ -2,3 +2,8 @@ import openpyxl
 from openpyxl.chart import Reference, BarChart
 from openpyxl.chart.axis import ChartLines
 from openpyxl.chart.layout import Layout, ManualLayout
+
+filename = 'video2.xlsx'
+wb  =openpyxl.load_workbook(filename)
+ws = wb['Total Sales by Genre']
+
