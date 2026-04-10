49b28cf0ef8b9b7b50890d100462515e641fdf3b
Szkolenie_54 <rajkonkret660@gmail.com>
2026-04-10 10:25:46 +0200

    [2 21] update ex7

--- day2/ex7.py
+++ day2/ex7.py
@@ -4,7 +4,32 @@ from openpyxl.chart.axis import ChartLines
 from openpyxl.chart.layout import Layout, ManualLayout
 
 filename = 'video2.xlsx'
-wb  =openpyxl.load_workbook(filename)
+wb = openpyxl.load_workbook(filename)
 ws = wb['Total Sales by Genre']
 
 # dane do wykresu
+values = Reference(
+    ws,
+    min_col=2,
+    max_col=2,
+    min_row=1,
+    max_row=13
+)
+
+cats = Reference(
+    ws,
+    min_col=1,
+    max_col=1,
+    min_row=2,
+    max_row=13
+)
+
+# tworzenie wykresu
+chart = BarChart()
+chart.add_data(values, titles_from_data=True)
+chart.set_categories(cats)
+
+ws.add_chart(chart, 'D2')
+
+wb.save(filename)
+wb.close()
