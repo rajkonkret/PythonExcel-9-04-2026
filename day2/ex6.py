0f6d00106bb660daa4800d3d0f7b2ca05f12fa8c
Szkolenie_54 <rajkonkret660@gmail.com>
2026-04-10 09:59:00 +0200

    [2 17] update ex6

--- day2/ex6.py
+++ day2/ex6.py
@@ -26,9 +26,37 @@ wb.close()
 
 print(ws['S2'].value)  # =COUNTIF(E2:E16328, "Sports")
 
+# zsumanie sprzedazy dla spełniających warunek
 ws['T1'] = "Total sum of Sports sales"
 ws['T2'] = '=SUMIF(E2:E16328, "Sports", K2:K16328)'
 # =SUMA.JEŻELI(E2:E16328; "Sports"; K2:K16328)
 
 wb.save(filename)
 wb.close()
+
+# zaokrąglenie do najbliższego górnego 25
+ws['U1'] = "Rounded sum of Sports Sales"
+ws['U2'] = '=CEILING(T2, 25)'
+
+wb.save(filename)
+wb.close()
+
+# zaokrąglenie do najbliższego górnego całkowitego
+ws['V1'] = "Rounded sum of Sports Sales"
+ws['V2'] = '=CEILING(T2, 1)'
+# =ZAOKR.W.GÓRĘ(T2; 1)
+wb.save(filename)
+wb.close()
+
+# w dół
+ws['X1'] = "FLOOR"
+ws['X2'] = '=FLOOR(T2, 25)'
+wb.save(filename)
+wb.close()
+# =ZAOKR.W.DÓŁ(T2; 25)
+
+ws['W1'] = "Rounded"
+ws['W2'] = '=ROUND(T2, 0)'
+wb.save(filename)
+wb.close()
+# =ZAOKR(T2; 0)
