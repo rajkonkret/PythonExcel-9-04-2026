d9a31dc9126586c8e2cec61be61349cb6f3b4e56
Szkolenie_54 <rajkonkret660@gmail.com>
2026-04-10 11:03:16 +0200

    [2 29] update ex9

---
+++ day2/ex9.py
@@ -0,0 +1,25 @@
+import openpyxl
+
+from openpyxl.styles import Font, colors, PatternFill, Border, Side
+from openpyxl.formatting.rule import  CellIsRule
+from openpyxl.styles.fills import FILL_PATTERN_DARKUP
+
+# FILL_NONE = 'none'
+# FILL_SOLID = 'solid'
+# FILL_PATTERN_DARKDOWN = 'darkDown'
+# FILL_PATTERN_DARKGRAY = 'darkGray'
+# FILL_PATTERN_DARKGRID = 'darkGrid'
+# FILL_PATTERN_DARKHORIZONTAL = 'darkHorizontal'
+# FILL_PATTERN_DARKTRELLIS = 'darkTrellis'
+# FILL_PATTERN_DARKUP = 'darkUp'
+# FILL_PATTERN_DARKVERTICAL = 'darkVertical'
+# FILL_PATTERN_GRAY0625 = 'gray0625'
+# FILL_PATTERN_GRAY125 = 'gray125'
+# FILL_PATTERN_LIGHTDOWN = 'lightDown'
+# FILL_PATTERN_LIGHTGRAY = 'lightGray'
+# FILL_PATTERN_LIGHTGRID = 'lightGrid'
+# FILL_PATTERN_LIGHTHORIZONTAL = 'lightHorizontal'
+# FILL_PATTERN_LIGHTTRELLIS = 'lightTrellis'
+# FILL_PATTERN_LIGHTUP = 'lightUp'
+# FILL_PATTERN_LIGHTVERTICAL = 'lightVertical'
+# FILL_PATTERN_MEDIUMGRAY = 'mediumGray'
