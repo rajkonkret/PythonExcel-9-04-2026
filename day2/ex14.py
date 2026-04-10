43285f1a9b119bf926bed458c85b5c6a904be014
Szkolenie_54 <rajkonkret660@gmail.com>
2026-04-10 12:59:54 +0200

    [2 49] update ex14

---
+++ day2/ex14.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+df = pd.DataFrame(
+    {
+        'Imie': ['Jan', 'Anna', 'Tomek'],
+        "Wiek": [29, 24, 35],
+        "Miasto": ['Warszawa', "Wrocław", "Legnica"]
+    }
+)
