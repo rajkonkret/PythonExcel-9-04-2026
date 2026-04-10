d8bb61cbd3ad6cc505f72bfe6a1636ea6737dd8f
Szkolenie_54 <rajkonkret660@gmail.com>
2026-04-10 10:49:59 +0200

    [2 25] update ex8

---
+++ day2/ex8.py
@@ -0,0 +1,36 @@
+import openpyxl
+
+from openpyxl.styles import Font, colors, PatternFill, Border, Side
+
+filename = 'video2.xlsx'
+wb = openpyxl.load_workbook(filename)
+ws = wb.active
+
+print(ws.title)  # vgsales
+
+# zmiana nazwy arkusza
+# ws.title = "Video Games Sales Data"
+#
+# wb.save(filename)
+# wb.close()
+
+sheet_names = wb.sheetnames
+print(sheet_names)
+# ['Video Games Sales Data', 'Total Sales by Genre', 'Breakdown of Sales by Genre', 'Breakdown of Sales by Year']
+
+sheet = wb[sheet_names[1]]  # indeks 1, arkusz numer 2
+wb.active = wb.index(sheet)
+
+# wb.save(filename)
+# wb.close()
+
+ws = wb.active
+print(ws.title)  # Total Sales by Genre
+
+# formatowanie komórek
+ws = wb['Video Games Sales Data']
+
+ws['A1'].font = Font(bold=True, size=12)
+
+wb.save(filename)
+wb.close()
