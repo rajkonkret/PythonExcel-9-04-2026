3b71fbf9318041f7c140bfc303f3bfebdc9ec93f
Szkolenie_54 <rajkonkret660@gmail.com>
2026-04-10 09:09:21 +0200

    [2 9] update ex5

--- day2/ex5.py
+++ day2/ex5.py
@@ -24,3 +24,9 @@ ws = wb['vgsales']
 values = [ws.cell(row=ws.max_row, column=i).value for i in range(1, ws.max_column + 1)]
 print(values)
 # [1, 'The Legend of Zelda', 'Wii', 1986, 'Action', 'Nintendo', 3.74, 0.93, 1.69, 0.14, 6.51]
+
+# usunięcie ostatniego wiersza
+ws.delete_rows(ws.max_row, 1)  # jeden ostatni
+
+wb.save(filename)
+wb.close()
