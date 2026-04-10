7e265476bca32a1663c7b6c04f72d6b2631b5a15
Szkolenie_54 <rajkonkret660@gmail.com>
2026-04-10 09:31:41 +0200

    [2 14] update ex6

--- day2/ex6.py
+++ day2/ex6.py
@@ -7,14 +7,19 @@ ws = wb['vgsales']
 
 ws['P1'] = "Averages Sales"
 ws['P2'] = '=AVERAGE(K2:K16328)'
-
+# =ŚREDNIA(K2:K16328)
 wb.save(filename)
 wb.close()
 
 ws['Q1'] = "Number of populated cells"
 ws['Q2'] = '=COUNTA(E2:E16328)'
-
+# =ILE.NIEPUSTYCH(E2:E16328)
 wb.save(filename)
 wb.close()
 
+ws["S1"] = "Total Sports Sales"
+ws['S2'] = '=COUNTIF(E2:E16328, "Sports")'
+# =LICZ.JEŻELI(E2:E16328; "Sports")
 
+wb.save(filename)
+wb.close()
