572260cfcafff195cc64f8e8d1d16c5f4d8d146b
Szkolenie_54 <rajkonkret660@gmail.com>
2026-04-10 14:00:31 +0200

    [2 59] update ex22

--- day2/ex22_pyexcel.py
+++ day2/ex22_pyexcel.py
@@ -10,3 +10,23 @@ data = [
 
 sheet = pyexcel.Sheet(data)
 sheet.save_as('wyniki.xlsx')
+
+# wczytanie
+sheet = pyexcel.get_sheet(file_name='wyniki.xlsx')
+print(sheet)
+# pyexcel sheet:
+# +-------+------+
+# | Imie  | Wiek |
+# +-------+------+
+# | Tomek | 38   |
+# +-------+------+
+# | Kasia | 34   |
+# +-------+------+
+
+print(sheet.columns())
+# <generator object Matrix.columns at 0x000001924217A960>
+
+for i in sheet.columns():
+    print(i)
+    # ['Imie', 'Tomek', 'Kasia']
+    # ['Wiek', '38', '34']
