e309be5fe3515e30fcd2689af20775bdf6f57ba7
Szkolenie_54 <rajkonkret660@gmail.com>
2026-04-10 12:07:19 +0200

    [2 36] update ex11

--- day2/ex11.py
+++ day2/ex11.py
@@ -2,3 +2,27 @@
 # Fast and versatile, the NumPy vectorization, indexing,
 # and broadcasting concepts are the de-facto standards of array computing today.
 
+import math
+
+import numpy as np
+
+# pip install numpy
+
+# ndarray - tblice, macierze
+
+array = np.array([10, 100, 1000.])
+print(array)  # [  10.  100. 1000.]
+print(array.dtype)  # float64
+
+array2 = np.array([[1, 2, 3],
+                   [4, 5, 6]])
+
+print(array2)
+# [[1 2 3]
+#  [4 5 6]]
+
+print(array2.dtype)  # int64
+
+# rzutowanie na typy pythonowe
+print(float(array[0]))  # 10.0
+print(type(float(array[0])))  # <class 'float'>
