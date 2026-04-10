4d7dbab9ed827053440bb8a14d98bec11a9de5ca
Szkolenie_54 <rajkonkret660@gmail.com>
2026-04-10 12:26:48 +0200

    [2 38] update ex12

---
+++ day2/ex12.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+data = pd.read_excel('course_participants.xlsx')
+print(data)
+#    numer   imię  wiek    kraj  ocena kontynent
+# 0   1001   Mark    55  Włochy    4.5    Europa
+# 1   1000   John    33     USA    6.7   Ameryka
+# 2   1002    Tim    41     USA    3.9   Ameryka
+# 3   1003  Jenny    12  Niemcy    9.0    Europa
