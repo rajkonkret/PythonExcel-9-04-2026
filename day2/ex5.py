fd53dab0ebb5666b17dc9102c31bec7092f24376
Szkolenie_54 <rajkonkret660@gmail.com>
2026-04-10 09:02:56 +0200

    [2 8] update ex5

--- day2/ex5.py
+++ day2/ex5.py
@@ -19,6 +19,8 @@ wb = openpyxl.load_workbook(filename)
 ws = wb.active
 
 ws = wb['vgsales']
+
+# wypisanie ostatniego wiersza
 values = [ws.cell(row=ws.max_row, column=i).value for i in range(1, ws.max_column + 1)]
 print(values)
 # [1, 'The Legend of Zelda', 'Wii', 1986, 'Action', 'Nintendo', 3.74, 0.93, 1.69, 0.14, 6.51]
