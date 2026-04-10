53b48e74f91ff75949c155244e36fed7c7bb4375
Szkolenie_54 <rajkonkret660@gmail.com>
2026-04-10 09:02:00 +0200

    [2 7] update ex5

--- day2/ex5.py
+++ day2/ex5.py
@@ -21,3 +21,4 @@ ws = wb.active
 ws = wb['vgsales']
 values = [ws.cell(row=ws.max_row, column=i).value for i in range(1, ws.max_column + 1)]
 print(values)
+# [1, 'The Legend of Zelda', 'Wii', 1986, 'Action', 'Nintendo', 3.74, 0.93, 1.69, 0.14, 6.51]
